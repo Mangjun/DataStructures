07874cd172e4b8772e7c68cf63914988d9824779
Mangjun <myoungjun55@naver.com>
2023-09-29 22:13:20 +0900

    최적화

--- Code/src/solution/MyArrayList.java
+++ Code/src/solution/MyArrayList.java
@@ -6,6 +6,13 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
+/**
+ * 알고리즘
+ * 상수 시간: 실행 시간이 입력 크기에 의존 하지 않음
+ * 선형: 실행 시간이 입력의 크기에 비례 
+ * 이차: 실행 시간이 n²에 비례
+ * 대부분의 경우 반복문이 1개라면 선형, 반복문 2개가 중첩되었다면 이차
+ */
 public class MyArrayList<T> implements List<T> {
 
     private int size; // 데이터 개수
@@ -17,6 +24,11 @@ public class MyArrayList<T> implements List<T> {
         size = 0;
     }
 
+    /**
+     * n번 추가하면 요소 n개를 추가하고 n-2개를 복사
+     * 총 연산 횟수: 2n - 2
+     * 평균: 2 - 2/n
+     */
     @SuppressWarnings("unchecked")
     @Override
     public boolean add(T element) {
@@ -25,13 +37,11 @@ public class MyArrayList<T> implements List<T> {
             T[] bigArray = (T[]) new Object[array.length * 2];
             System.arraycopy(array, 0, bigArray, 0, array.length);
             array = bigArray;
-        }
-
+        } // 선형
         array[size] = element;
         size++;
-
         return true;
-    }
+    } // 상수 시간
 
     @Override
     public void add(int index, T element) {
@@ -46,7 +56,7 @@ public class MyArrayList<T> implements List<T> {
         }
 
         array[index] = element;
-    }
+    } // 선형
 
     @Override
     public boolean addAll(Collection<? extends T> collection) {
@@ -88,17 +98,17 @@ public class MyArrayList<T> implements List<T> {
             throw new IndexOutOfBoundsException();
         }
         return array[index];
-    }
+    } // 상수 시간
 
     @Override
     public int indexOf(Object target) {
         for(int i=0; i<size; i++) {
-            if(array[i].equals(target)) {
+            if(equals(target, array[i])) {
                 return i;
             }
         }
         return -1;
-    }
+    } // 선형
 
     private boolean equals(Object target, Object element) {
         if (target == null) {
@@ -106,7 +116,7 @@ public class MyArrayList<T> implements List<T> {
         } else {
             return target.equals(element);
         }
-    }
+    } // 상수 시간
 
     @Override
     public boolean isEmpty() {
@@ -160,21 +170,13 @@ public class MyArrayList<T> implements List<T> {
 
     @Override
     public T remove(int index) {
-        if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException();
-        }
-
-        T element = array[index];
-
-        for(int i=index; i<size; i++) {
+       T element = get(index);
+        for(int i=index; i<size-1; i++) {
             array[i] = array[i+1];
         }
-
-        array[size] = null;
         size--;
-
         return element;
-    }
+    } // 선형(마지막 요소일 경우 상수 시간)
 
     @Override
     public boolean removeAll(Collection<?> collection) {
@@ -183,7 +185,7 @@ public class MyArrayList<T> implements List<T> {
             flag &= remove(obj);
         }
         return flag;
-    }
+    } // collection의 크기가 상수: 선형, collection의 크기가 배열의 요소에 비례: 이차
 
     @Override
     public boolean retainAll(Collection<?> collection) {
@@ -192,13 +194,10 @@ public class MyArrayList<T> implements List<T> {
 
     @Override
     public T set(int index, T element) {
-        if(index < 0 || index > size) {
-            throw new IndexOutOfBoundsException();
-        }
-
+        T old = get(index); // get()에서 예외 처리
         array[index] = element;
-        return element;
-    }
+        return old;
+    } // 상수 시간
 
     @Override
     public int size() {

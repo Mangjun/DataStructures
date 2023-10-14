dbd9913e17b46a9c03db660f838c320831ae64be
Mangjun <myoungjun55@naver.com>
2023-10-14 13:15:40 +0900

    [*] Code 최적화

--- Code/src/solution/MyTreeMap.java
+++ Code/src/solution/MyTreeMap.java
@@ -39,26 +39,37 @@ public class MyTreeMap<K, V> implements Map<K, V> {
             throw new IllegalArgumentException();
         }
 
+        // 컴파일러 경고 무시
         @SuppressWarnings("unchecked")
         Comparable<? super K> k = (Comparable<? super K>) target;
 
-        Node findNode = root;
-
-        while(findNode != null) {
-            // target이 더 클 시
-            if(k.compareTo(findNode.key) > 0) {
-                findNode = findNode.right;
-            }
-            // target이 더 작을 시
-            else if(k.compareTo(findNode.key) < 0) {
-                findNode = findNode.left;
-            }
-            // 같을 때
-            else {
-                return findNode;
-            }
+        // 실제 탐색
+        // 최적화 코드
+        Node node = root;
+        while(node != null) {
+            int cmp = k.compareTo(node.key);
+            if(cmp < 0)
+                node = node.left;
+            else if(cmp > 0)
+                node = node.right;
+            else
+                return node;
+        // 내가 짠 코드
+//        Node findNode = root;
+//        while(findNode != null) {
+//            // target이 더 클 시
+//            if(k.compareTo(findNode.key) > 0) {
+//                findNode = findNode.right;
+//            }
+//            // target이 더 작을 시
+//            else if(k.compareTo(findNode.key) < 0) {
+//                findNode = findNode.left;
+//            }
+//            // 같을 때
+//            else {
+//                return findNode;
+//            }
         }
-
         return null;
     }
 
@@ -75,11 +86,30 @@ public class MyTreeMap<K, V> implements Map<K, V> {
     }
 
     private boolean containsValueHelper(Node node, Object target) {
-        for (V value : values()) {
-            if(equals(target, value)) {
-                return true;
-            }
+        // 최적화 코드
+        // 재귀의 기저 사례를 검사
+        if(node == null) {
+            return false;
         }
+        // 원하는 것을 찾았는지 확인
+        if(equals(target, node.value)) {
+            return true;
+        }
+        // 왼쪽 하위 트리에서 target을 찾는 재귀호출
+        if(containsValueHelper(node.left, target)) {
+            return true;
+        }
+        // 오른쪽 하위 트리에서 target을 찾는 재귀호출
+        if(containsValueHelper(node.right, target)) {
+            return true;
+        }
+        // 내가 짠 코드
+//        for (V value : values()) {
+//            if(equals(target, value)) {
+//                return true;
+//            }
+//        }
+//        return false;
         return false;
     }
 
@@ -104,17 +134,30 @@ public class MyTreeMap<K, V> implements Map<K, V> {
 
     @Override
     public Set<K> keySet() {
-        Set<K> set = new LinkedHashSet<K>();
-        Deque<Node> stack = new LinkedList<Node>();
-        stack.push(root);
-        while (!stack.isEmpty()) {
-            Node node = stack.pop();
-            if (node == null) continue;
-            set.add(node.key);
-            stack.push(node.left);
-            stack.push(node.right);
-        }
+        // 최적화 코드
+        Set<K> set = new LinkedHashSet<>();
+        addInOrder(root, set);
         return set;
+        // 내가 짠 코드
+//        Set<K> set = new LinkedHashSet<K>();
+//        Deque<Node> stack = new LinkedList<Node>();
+//        stack.push(root);
+//        while (!stack.isEmpty()) {
+//            Node node = stack.pop();
+//            if (node == null) continue;
+//            set.add(node.key);
+//            stack.push(node.left);
+//            stack.push(node.right);
+//        }
+//        return set;
+    }
+
+    // 트리의 중위 순회
+    private void addInOrder(Node node, Set<K> set) {
+        if(node == null) return;
+        addInOrder(node.left, set);
+        set.add(node.key);
+        addInOrder(node.right, set);
     }
 
     @Override
@@ -137,36 +180,62 @@ public class MyTreeMap<K, V> implements Map<K, V> {
      * @return key가 이미 있다면 기존 값을 반환, key가 없다면 null을 반환
      */
     private V putHelper(Node node, K key, V value) {
-        Node oldNode = findNode(key);
-        if(oldNode == null) {
-            Node newNode = makeNode(key, value);
-            Node findNode = root;
-            Comparable<? super K> k = (Comparable<? super K>) key;
-            while(findNode != null) {
-                if(k.compareTo(findNode.key) > 0) {
-                    if(findNode.right != null) {
-                        findNode = findNode.right;
-                    } else {
-                        findNode.right = newNode;
-                        size++;
-                        return null;
-                    }
-                } else {
-                    if(findNode.left != null) {
-                        findNode = findNode.left;
-                    } else {
-                        findNode.left = newNode;
-                        size++;
-                        return null;
-                    }
-                }
+        // 최적화 코드
+        Comparable<? super K> k = (Comparable<? super K>) key;
+        int cmp = k.compareTo(node.key);
+
+        if(cmp < 0) {
+            if(node.left == null) {
+                node.left = new Node(key, value);
+                size++;
+                return null;
+            } else {
+                return putHelper(node.left, key, value);
             }
-            return null;
-        } else {
-            V oldValue = oldNode.value;
-            oldNode.value = value;
-            return oldValue;
         }
+        if(cmp > 0) {
+            if(node.right == null) {
+                node.right = new Node(key, value);
+                size++;
+                return null;
+            } else {
+                return putHelper(node.right, key, value);
+            }
+        }
+        V oldValue = node.value;
+        node.value = value;
+        return oldValue;
+        // 내가 짠 코드
+//        Node oldNode = findNode(key);
+//        if(oldNode == null) {
+//            Node newNode = makeNode(key, value);
+//            Node findNode = root;
+//            Comparable<? super K> k = (Comparable<? super K>) key;
+//            while(findNode != null) {
+//                if(k.compareTo(findNode.key) > 0) {
+//                    if(findNode.right != null) {
+//                        findNode = findNode.right;
+//                    } else {
+//                        findNode.right = newNode;
+//                        size++;
+//                        return null;
+//                    }
+//                } else {
+//                    if(findNode.left != null) {
+//                        findNode = findNode.left;
+//                    } else {
+//                        findNode.left = newNode;
+//                        size++;
+//                        return null;
+//                    }
+//                }
+//            }
+//            return null;
+//        } else {
+//            V oldValue = oldNode.value;
+//            oldNode.value = value;
+//            return oldValue;
+//        }
     }
 
     @Override
@@ -178,8 +247,41 @@ public class MyTreeMap<K, V> implements Map<K, V> {
 
     @Override
     public V remove(Object key) {
-        // OPTIONAL TODO: FILL THIS IN!
-        throw new UnsupportedOperationException();
+        Node node = findNode(key);
+        // 1번째 경우 - 단말 노드
+        if(node.left == null && node.right == null) {
+            V oldValue = node.value;
+            node = null;
+            return oldValue;
+        }
+        // 2번째 경우 - 자식이 1개인 노드
+        if(node.left == null && node.right != null) {
+            V oldValue = node.value;
+            node = node.left;
+            return oldValue;
+        }
+
+        if(node.left != null && node.right == null) {
+            V oldValue = node.value;
+            node = node.right;
+            return oldValue;
+        }
+        // 3번째 경우 - 자식이 2개인 노드
+        if(node.left != null && node.right != null) {
+            V oldValue = node.value;
+//            Comparable<? super V> v = (Comparable<? super V>) oldValue;
+//            List<V> value = (List<V>) values();
+//            int min = v.compareTo(value.get(0));
+//            V minValue = value.get(0);
+//            for(V var : value) {
+//                int cmp = v.compareTo(var);
+//                if(min > cmp) {
+//                    minValue = var;
+//                }
+//            }
+            return oldValue;
+        }
+        return null;
     }
 
     @Override
